3b819ea2ee5b2bba7547c1ec9eee8abf616dc76a
Bulbariss <45638123+Bulbariss@users.noreply.github.com>
2019-10-24 19:28:38 +0300

    Add files via upload

--- app.js
+++ app.js
@@ -1,10 +1,10 @@
 console.clear();
 
+// Menu
 var app = function () {
   var body;
   var menu;
   var menuItems;
-
   var init = function init() {
     body = document.querySelector('body');
     menu = document.querySelector('.menu-icon');
@@ -16,6 +16,11 @@ var app = function () {
     menu.addEventListener('click', function () {
       return toggleClass(body, 'nav-active');
     });
+    for (var i = 0; i < menuItems.length; i++) {
+      menuItems[i].addEventListener('click', function () {
+        return toggleClass(body, 'nav-active');
+      });
+    }
   };
 
   var toggleClass = function toggleClass(element, stringClass) {
@@ -24,3 +29,25 @@ var app = function () {
 
   init();
 }();
+
+// Get the current year for the copyright
+$('#year').text(new Date().getFullYear());
+
+// Init Scrollspy
+$('body').scrollspy({ target: '#main-nav' });
+
+// Smooth Scrolling
+$("a").on('click', function (event) {
+  if (this.hash !== "") {
+    event.preventDefault();
+
+    const hash = this.hash;
+
+    $('html, body').animate({
+      scrollTop: $(hash).offset().top
+    }, 800, function () {
+
+      window.location.hash = hash;
+    });
+  }
+});
