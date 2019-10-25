30b037b8ad5d8523e4aa51e500669160099a0857
Bulbariss <45638123+Bulbariss@users.noreply.github.com>
2019-10-25 17:25:38 +0300

    Add files via upload

--- app.js
+++ app.js
@@ -1,5 +1,3 @@
-console.clear();
-
 // Menu
 var app = function () {
   var body;
@@ -30,6 +28,54 @@ var app = function () {
   init();
 }();
 
+// Cubic-Bezier for jquery
+(function(factory) {
+  if (typeof exports === "object") {
+    factory(require("jquery"));
+  } else if (typeof define === "function" && define.amd) {
+    define(["jquery"], factory);
+  } else {
+    factory(jQuery);
+  }
+}(function($) {
+  $.extend({ bez: function(encodedFuncName, coOrdArray) {
+    if ($.isArray(encodedFuncName)) {
+      coOrdArray = encodedFuncName;
+      encodedFuncName = 'bez_' + coOrdArray.join('_').replace(/\./g, 'p');
+    }
+    if (typeof $.easing[encodedFuncName] !== "function") {
+      var polyBez = function(p1, p2) {
+        var A = [null, null], B = [null, null], C = [null, null],
+            bezCoOrd = function(t, ax) {
+              C[ax] = 3 * p1[ax], B[ax] = 3 * (p2[ax] - p1[ax]) - C[ax], A[ax] = 1 - C[ax] - B[ax];
+              return t * (C[ax] + t * (B[ax] + t * A[ax]));
+            },
+            xDeriv = function(t) {
+              return C[0] + t * (2 * B[0] + 3 * A[0] * t);
+            },
+            xForT = function(t) {
+              var x = t, i = 0, z;
+              while (++i < 14) {
+                z = bezCoOrd(x, 0) - t;
+                if (Math.abs(z) < 1e-3) break;
+                x -= z / xDeriv(x);
+              }
+              return x;
+            };
+        return function(t) {
+          return bezCoOrd(xForT(t), 1);
+        }
+      };
+      $.easing[encodedFuncName] = function(x, t, b, c, d) {
+        return c * polyBez([coOrdArray[0], coOrdArray[1]], [coOrdArray[2], coOrdArray[3]])(t/d) + b;
+      }
+    }
+    return encodedFuncName;
+  }});
+}));
+
+
+
 // Get the current year for the copyright
 $('#year').text(new Date().getFullYear());
 
@@ -45,9 +91,30 @@ $("a").on('click', function (event) {
 
     $('html, body').animate({
       scrollTop: $(hash).offset().top
-    }, 800, function () {
+    }, 900, $.bez([0.39, 0.575, 0.565, 1]), function () {
 
       window.location.hash = hash;
+      
     });
   }
 });
+
+/* When the user scrolls down, hide the navbar. When the user scrolls up, show the navbar */
+var prevScrollpos = window.pageYOffset;
+window.onscroll = function() {
+  var currentScrollPos = window.pageYOffset;
+  if (prevScrollpos > currentScrollPos) {
+    document.getElementById("navbar").style.top = "0";
+  } else {
+    document.getElementById("navbar").style.top = "-45px";
+  }
+  prevScrollpos = currentScrollPos;
+}
+
+
+// {
+//   duration: 800,
+//   specialEasing: {
+//     width: "linear",
+//     height: "easeOutBounce"
+//   },
