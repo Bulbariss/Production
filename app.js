14e76c7e70d84936490261621f6b315d1ff3833a
Bulbariss <45638123+Bulbariss@users.noreply.github.com>
2019-10-26 22:06:33 +0300

    Add files via upload

--- app.js
+++ app.js
@@ -124,15 +124,3 @@ $("a.nav-link, a.smooth-scr").on('click', function (event) {
     });
   }
 });
-
-// /* When the user scrolls down, hide the navbar. When the user scrolls up, show the navbar */
-// var prevScrollpos = window.pageYOffset;
-// window.onscroll = function() {
-//   var currentScrollPos = window.pageYOffset;
-//   if (prevScrollpos > currentScrollPos) {
-//     document.getElementById("navbar").style.top = "0";
-//   } else {
-//     document.getElementById("navbar").style.top = "-45px";
-//   }
-//   prevScrollpos = currentScrollPos;
-// }
